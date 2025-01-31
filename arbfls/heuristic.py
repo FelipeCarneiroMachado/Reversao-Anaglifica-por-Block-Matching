70a4869910e9858fddc1f41c5acb743aa99e5e21
FelipeCarneiroMachado <felipecmachado05@gmail.com>
2025-01-31 16:26:51 -0300

    comentarios

--- arbfls/heuristic.py
+++ arbfls/heuristic.py
@@ -13,33 +13,60 @@ Heuristicas para complementatr a minimizacao da SAD baseando-se na posicao relat
 Heuristicas implementadas:
 -Flat: assume qe os matches corretos estarao todos para o mesmo lado,
     assim penaliza os que estao para o lado contrario, e premia os que estao para o lado "correto"
-    *params => {"alpha" : number >= 0 } forca da penalizacao e premiacao
+    *params => {
+        "alpha" : number >= 0 (forca da penalizacao e premiacao) 
+    } 
 -Picos discretos: baseia-se no fato de que a maioria dos matches tem um deslocamento proximo,
     beneficia blocos em um numero fixo de posicoes, escolhidas pela frequencia de matches.
     Aumenta o tempo de execucao pois requer uma passagem anterior do algoritmo para determinar as frequencias
-    *params => {
-            
+    *params => { 
+        "alpha" : numero >= 0 (forca da penalizacao e premiacao da heuristica flat),
+        "beta" : numero >= 0 (forca da penalizacao dos picos discretos),
+        "npeaks" : numero >= 0 (quantidade de picos a serem considerados)
+    }       
 """
 
 
-
-def discrete_points_preprocess(edge_left:np.ndarray, edge_right:np.ndarray,config : dict = config_dict) -> tuple[np.ndarray, np.ndarray]:
-    dimensions = edge_left.shape
+"""
+Pre processamento para heuristica de picos discretos e janela_dinamica, busca os deslocamentos de bloco mais freqentes
+e retorna o histograma
+Parametros:
+    pre_processed_left: canal esquerdo com pre-processamento aplicado
+    pre_processed_right: canal direito com pre-processamento aplicado
+    config: dicionario de configuracao, config.config_dict por padrao
+Retorno:
+    histograma dos deslocamentos
+"""
+def compute_match_histogram(pre_processed_left:np.ndarray, pre_processed_right:np.ndarray,config : dict = config_dict) -> tuple[np.ndarray, np.ndarray]:
+    dimensions = pre_processed_left.shape
+    # Histograma dos matches realizados, armazena os deslocamentos relativos
     histogram = np.zeros(((2 *config["horizontal_window"]) + 1,))
+    # Itera sobre a imagem
     for y in range(0, dimensions[0], config["block_size"]):
         for x in range(0, dimensions[1], config["block_size"]):
-            l_match, r_match =  minimize_sad(edge_left, edge_right, dimensions, y, x, config)
+            # Realiza o match com heuristica flat
+            l_match, r_match =  minimize_sad(pre_processed_left, pre_processed_right, dimensions, y, x, config)
             histogram[l_match[1] - x + config["horizontal_window"]] += 1
-    
-    mulist = list(map(lambda x : x - 16,np.argpartition(histogram, -config["heuristic_params"]["npeaks"])[-config["heuristic_params"]["npeaks"]:]))
+
+    # Transformacao em probabilidades
     histogram = histogram / np.sum(histogram)
-    # for i, v in enumerate(histogram):
-    #     print(f"{i - config["horizontal_window"]} -> {v:.3f}")
-    return mulist, histogram
-    
+    return histogram
+
+"""
+Computa a lista de deslocamentos para picos discretos com base no histograma
+Parametros:
+    histogram: histograma de deslocamentos computado por compute_match_histogram
+    config: dicionario de configuracao, config.config_dict por padrao
+Retorno:
+    lista de deslocamentos
+"""
+def get_point_list(histogram, config:dict = config_dict) -> list:
+    return list(map(lambda x : x - 16,np.argpartition(histogram, -config["heuristic_params"]["npeaks"])[-config["heuristic_params"]["npeaks"]:]))
+
+
 def discrete_points_inmatch(y:int,x:int, config:dict = config_dict) -> int :
     if x <= 0:
-        if x in config["heuristic_variables"]["mulist"]:
+        if x in config["heuristic_variables"]["point_list"]:
             return -config["heuristic_params"]["alpha"] - config["heuristic_params"]["beta"]    
         else:
             return -config["heuristic_params"]["alpha"]
@@ -47,26 +74,28 @@ def discrete_points_inmatch(y:int,x:int, config:dict = config_dict) -> int :
         return config["heuristic_params"]["alpha"]
 
 
-def find_window_size(edge_left:np.ndarray, edge_right:np.ndarray,config : dict = config_dict):
-    dp_config = config.copy()
-    dp_config["heuristic"] = "flat"
-    dp_config["heuristic_params"]["alpha"] = 1000
-    mulist, histogram = discrete_points_preprocess(edge_left, edge_right, dp_config)
-    dw_param = config["dw_config"]
+def find_window_size(histogram ,config : dict = config_dict):
+    dw_param = config["dw_config"] # Encurtar expressoes
+    # Itera pelo histograma ate encontrar uma posicao acima de um limiar de significancia
     for i in range(len(histogram)):
         if histogram[i] >= dw_param["threshold"]:
-            config["horizontal_window"] = round((config["horizontal_window"] - i) * dw_param["extension"])
-            break
-    return mulist
+            # Retorna o deslocamento multiplicado por uma folga
+            return round((config["horizontal_window"] - i) * dw_param["extension"])
+    # Nao deve chegar ate esta linha
+    return config["horizontal_window"]
 
 
-#Heuristic preprocessing function, the parameters are specified in the heuristic_params disctionary in the config_dict
-#Other parameters must be passed via kwargs
-def preprocess(edge_left:np.ndarray, edge_right:np.ndarray,config : dict = config_dict, **kwargs) -> dict | None:
-    if config["dinamic_window"]:
-        mulist = find_window_size(edge_left, edge_right, config)
+
+def preprocess(edge_left:np.ndarray, edge_right:np.ndarray, config : dict = config_dict, **kwargs) -> dict | None:
+    if config["dinamic_window"] or config["heuristic"] == "discrete_points":
+        hist_config = config.copy()
+        hist_config["heuristic"] = "flat"
+        hist_config["heuristic_params"]["alpha"] = 1000
+        histogram = compute_match_histogram(edge_left, edge_right, hist_config)
+        if config["dinamic_window"]:
+            config["horizontal_window"] = find_window_size(histogram, config)
         if config["heuristic"] == "discrete_points":
-            return {"mulist" : mulist}
+            return {"point_list" : get_point_list(histogram, config)}
     match config["heuristic"]:
         case "flat":
             return None
@@ -74,7 +103,7 @@ def preprocess(edge_left:np.ndarray, edge_right:np.ndarray,config : dict = confi
             dp_config = config.copy()
             dp_config["heuristic"] = "flat"
             dp_config["heuristic_params"]["alpha"] = 1000
-            return {"mulist" : discrete_points_preprocess(edge_left, edge_right, dp_config)}
+            return {"point_list" : compute_match_histogram(edge_left, edge_right, dp_config)}
         case "none":
             return None
         case _:
