c15e9747ff8df3587eb555918fe3e2c5734edbd5
FelipeCarneiroMachado <felipecmachado05@gmail.com>
2025-01-31 16:45:51 -0300

    comentarios

--- arbfls/heuristic.py
+++ arbfls/heuristic.py
@@ -63,17 +63,14 @@ Retorno:
 def get_point_list(histogram, config:dict = config_dict) -> list:
     return list(map(lambda x : x - 16,np.argpartition(histogram, -config["heuristic_params"]["npeaks"])[-config["heuristic_params"]["npeaks"]:]))
 
-
-def discrete_points_inmatch(y:int,x:int, config:dict = config_dict) -> int :
-    if x <= 0:
-        if x in config["heuristic_variables"]["point_list"]:
-            return -config["heuristic_params"]["alpha"] - config["heuristic_params"]["beta"]    
-        else:
-            return -config["heuristic_params"]["alpha"]
-    else:
-        return config["heuristic_params"]["alpha"]
-
-
+"""
+Computa a extensao da janela de busca a partir do histograma
+Parametros:
+    histogram: histograma de deslocamentos computado por compute_match_histogram
+    config: dicionario de configuracao, config.config_dict por padrao
+Retorno:
+    nova extensao da janela de busca
+"""
 def find_window_size(histogram ,config : dict = config_dict):
     dw_param = config["dw_config"] # Encurtar expressoes
     # Itera pelo histograma ate encontrar uma posicao acima de um limiar de significancia
@@ -85,13 +82,21 @@ def find_window_size(histogram ,config : dict = config_dict):
     return config["horizontal_window"]
 
 
-
-def preprocess(edge_left:np.ndarray, edge_right:np.ndarray, config : dict = config_dict, **kwargs) -> dict | None:
+"""
+Realiza quaisquer etapas de pre processamento pertinentes a heuristica, alem da janela de busca dinamica
+Parametros:
+    pre_processed_left: canal esquerdo com pre-processamento
+    pre_processed_right: canal direito com pre-processamento
+    config: dicionario de configuracao, config.config_dict por padrao
+Retorno:
+    dicionario de variaveis da heuristica
+"""
+def preprocess(pre_process_left:np.ndarray, pre_process_right:np.ndarray, config : dict = config_dict, **kwargs) -> dict | None:
     if config["dinamic_window"] or config["heuristic"] == "discrete_points":
         hist_config = config.copy()
         hist_config["heuristic"] = "flat"
         hist_config["heuristic_params"]["alpha"] = 1000
-        histogram = compute_match_histogram(edge_left, edge_right, hist_config)
+        histogram = compute_match_histogram(pre_process_left, pre_process_right, hist_config)
         if config["dinamic_window"]:
             config["horizontal_window"] = find_window_size(histogram, config)
         if config["heuristic"] == "discrete_points":
@@ -99,18 +104,39 @@ def preprocess(edge_left:np.ndarray, edge_right:np.ndarray, config : dict = conf
     match config["heuristic"]:
         case "flat":
             return None
-        case "discrete_points":
-            dp_config = config.copy()
-            dp_config["heuristic"] = "flat"
-            dp_config["heuristic_params"]["alpha"] = 1000
-            return {"point_list" : compute_match_histogram(edge_left, edge_right, dp_config)}
         case "none":
             return None
         case _:
             raise Exception("Unsupported heuristic")
 
 
-#Function called as the heuristic per se, inside the block matching
+
+"""
+Computa a penalizacao/recompensa para cada deslocamento de bloco para a heuristica de picos discretos
+Parametros:
+    y / x: deslocamentos
+    config: dicionario de configuracao, config.config_dict por padrao
+Retorno:
+    valor a ser adicionado a SAD
+"""
+def discrete_points_inmatch(y:int,x:int, config:dict = config_dict) -> int :
+    if x <= 0:
+        if x in config["heuristic_variables"]["point_list"]:
+            return -config["heuristic_params"]["alpha"] - config["heuristic_params"]["beta"]
+        else:
+            return -config["heuristic_params"]["alpha"]
+    else:
+        return config["heuristic_params"]["alpha"]
+
+
+"""
+Computa a penalizacao/recompensa para cada deslocamento de bloco
+Parametros:
+    y / x: deslocamentos
+    config: dicionario de configuracao, config.config_dict por padrao
+Retorno:
+    valor a ser adicionado a SAD
+"""
 def in_match(y:int, x:int, config:dict = config_dict) -> int | float:
     match config["heuristic"]:
         case "flat":
@@ -122,16 +148,3 @@ def in_match(y:int, x:int, config:dict = config_dict) -> int | float:
             return discrete_points_inmatch(y, x, config)
         case _:
             return 0
-
-#Heuristic postprocessing function, the parameters are specified in the heuristic_params disctionary in the config_dict
-#Other parameters must be passed via kwargs
-def postprocessing(edge_left:np.ndarray, edge_right:np.ndarray,config:dict = config_dict, **kwargs) -> dict  | None:
-    match config["heuristic"]:
-        case "flat":
-            None
-        case "none":
-            return None
-        case "discrete_points":
-            return None
-        case _:
-            raise Exception("Unsupported heuristic")
